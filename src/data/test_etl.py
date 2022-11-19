bbb564208df73bed7d4b99ea3068924f2f3638f8
darehunt <42155557+darehunt@users.noreply.github.com>
2022-11-18 21:37:48 -0800

    test_etl save temps

--- src/data/test_etl.py
+++ src/data/test_etl.py
@@ -15,19 +15,35 @@
 
 
 
+from random import random
+from random import seed
+import pandas as pd
+import networkx as nx
+import numpy as np
+from itertools import product
+import pickle
+import json
+
+
 def create_rand_graphs(N, Cn, Cp, Op, q, s):
     # make clique
     cliq_graph = nx.gnp_random_graph(Cn, Cp, seed=s) # red
     # make outside of clique
     out_graph = nx.gnp_random_graph(N-Cn, Op, seed=s) # blue
-    relabel = {}
-    return cliq_graph, out_graph
+
+    # save seperate graphs as files
+    pickle.dump(cliq_graph, open('data/test/raw/cliq_graph.pickle', 'wb'))
+    pickle.dump(out_graph, open('data/test/raw/out_graph.pickle', 'wb'))
 
 
 
 
 
-def create_combined(N, Cn, Cp, Op, q, s, clique, outer):
+def create_combined(N, Cn, Cp, Op, q, s):
+    # load in seperate graphs
+    clique = pickle.load(open('data/test/raw/cliq_graph.pickle', 'rb'))
+    outer = pickle.load(open('data/test/raw/out_graph.pickle', 'rb'))
+    
     
     clique_nodes = clique.nodes()
     
@@ -36,33 +52,58 @@ def create_combined(N, Cn, Cp, Op, q, s, clique, outer):
     nx.relabel_nodes(outer, outer_dict_relabel, False)
     outer_nodes = outer.nodes()
     
+    # create ground truth JSON
+    ground_truths = {
+        "clique_nodes": clique_nodes,
+        "outer_nodes": outer_nodes
+    }
     
+    with open("data/test/temp/ground_truth.json", "w") as outfile:
+        json.dump(dictionary, outfile)
+    
+    
+    # create and save combined graph
     combined = nx.Graph()
     combined.add_nodes_from(clique_nodes)
     combined.add_edges_from(clique.edges())
     combined.add_nodes_from(outer_nodes)
     combined.add_edges_from(outer.edges())
     
-    return combined, clique_nodes, outer_nodes
+    pickle.dump(combined, open('data/test/raw/combined_separated.pickle', 'wb'))
+    
+
+    
 
 
 
 
 
-def create_combined_edges(N, Cn, Cp, Op, q, s, clique, outer, combined, clique_nodes, outer_nodes):
+def create_combined_edges(N, Cn, Cp, Op, q, s):
+    combined = pickle.load(open('data/test/raw/combined_separated.pickle', 'rb'))
+    
+    with open('data/test/temp/ground_truth.json', 'r') as openfile:
+        ground_truth = json.load(openfile)
+    
     seed(s)
-    combined.add_edges_from([ (u, v) for u, v in product(clique_nodes, outer_nodes) if random() < q ])
-    return combined, clique_nodes, outer_nodes
+    combined.add_edges_from([ (u, v) for u, v in product(ground_truth['clique_nodes'], ground_truth['outer_nodes']) if random() < q ])
+    pickle.dump(combined, open('data/test/temp/combined.pickle', 'wb'))
+
 
 
 
 
 
 def plot_graph(combined, clique_nodes, outer_nodes):
+    combined = pickle.load(open('data/test/temp/combined.pickle', 'rb'))
+    
+    with open('data/test/temp/ground_truth.json', 'r') as openfile:
+        ground_truth = json.load(openfile)
+    
+    
     pos = nx.spring_layout(combined)
     
-    nx.draw_networkx_nodes(combined, pos=pos, nodelist=clique_nodes, node_color='r', node_size=50)
-    nx.draw_networkx_nodes(combined, pos=pos, nodelist=outer_nodes, node_color='b', node_size=50)
+    nx.draw_networkx_nodes(combined, pos=pos, nodelist=ground_truth['clique_nodes'], node_color='r', node_size=50)
+    nx.draw_networkx_nodes(combined, pos=pos, nodelist=ground_truth['outer_nodes'], node_color='b', node_size=50)
     nx.draw_networkx_edges(combined, pos=pos)
     return plt.show() 
-plot_graph(combined, clique_nodes, outer_nodes)
+    
