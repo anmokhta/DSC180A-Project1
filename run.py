e8f651af220aec57fc8d9eba5396c35214166d8c
Andrew Mok <amokhtarzadeh22@gmail.com>
2022-11-29 23:25:14 -0800

    run.py notes

--- run.py
+++ run.py
@@ -32,6 +32,14 @@ def main(targets):
         clean()
     
     if 'test' in targets:
+        targets.extend(["test-data", "model"])
+        print("extended test-data & model!")
+
+    if 'all' in targets:
+        targets.extend(["data", "model"])
+        print("extended data & model!")
+    
+    if 'test-data' in targets:
         with open('config/test_etl.json') as fh:
             test_etl_config = json.load(fh)
         #check if pickles not exist in raw, if true run function below
@@ -40,7 +48,12 @@ def main(targets):
         create_combined(**test_etl_config)
         create_combined_edges(**test_etl_config)
 
+    if 'data' in targets:
+        print("This will make data prepped for the model! use src/data_etl.py for making functions")
         
+    if 'model' in targets:
+        print("This will check which dataset to load (if REAL data exists, that. otherwise check if test data exists, uest that) and run model")
+        # Maybe do above with data checker function idk
         
 if __name__ == '__main__':
     # run via:
