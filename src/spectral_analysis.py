6804ea77e4537e9267c2644d8649c95845e7082f
Andrew Mok <amokhtarzadeh22@gmail.com>
2022-12-06 04:28:29 -0800

    prints

--- src/spectral_analysis.py
+++ src/spectral_analysis.py
@@ -32,12 +32,15 @@ def return_sub_graphs(G):
 
 
 def spectral_cluster(G):
+    print("starting spectral cluster")
     A = nx.to_numpy_array(G)
     D = np.diag(A.sum(axis=1))
     # graph laplacian
     L = D-A
+    print("laplacian created")
     # eigenvalues and eigenvectors
     vals, vecs = np.linalg.eig(L)
+    print("eigs found")
     # sort these based on the eigenvalues
     vecs = vecs[:,np.argsort(vals)]
     vals = vals[np.argsort(vals)]
