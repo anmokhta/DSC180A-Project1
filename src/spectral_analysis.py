11c0a05c72d2435bedb1448cb42e90a1bf509610
Andrew Mok <amokhtarzadeh22@gmail.com>
2022-12-05 23:23:22 -0800

    graph_stats func

--- src/spectral_analysis.py
+++ src/spectral_analysis.py
@@ -1,6 +1,20 @@
 import numpy as np
 import networkx as nx
 
+def graph_stats(G):
+    deg = []
+    for i in G.degree:
+        deg.append(i[1])
+    
+    
+    stats = {
+        'nodes': G.order(),
+        'edges': G.number_of_edges(),
+        'weights': G.size(),
+        'avg_degree': sum(deg) / len(deg)
+    }
+    
+    return stats
 
 def spectral_cluster(G):
     A = nx.to_numpy_array(G)
@@ -13,27 +27,3 @@ def spectral_cluster(G):
     vecs = vecs[:,np.argsort(vals)]
     vals = vals[np.argsort(vals)]
     return [1 if i > 0 else 0 for i in vals]
-
-def create_original_data():
-
-
-def create_prediction_data():
-
-
-def create_ground_truth_data():
-
-
-
-# def plot_graph(combined, clique_nodes, outer_nodes):
-#     combined = pickle.load(open('data/test/temp/combined.pickle', 'rb'))
-#    
-#     with open('data/test/temp/ground_truth.json', 'r') as openfile:
-#         ground_truth = json.load(openfile)
-#
-#   
-#     pos = nx.spring_layout(combined)
-#    
-#     nx.draw_networkx_nodes(combined, pos=pos, nodelist=ground_truth['clique_nodes'], node_color='r', node_size=50)
-#     nx.draw_networkx_nodes(combined, pos=pos, nodelist=ground_truth['outer_nodes'], node_color='b', node_size=50)
-#     nx.draw_networkx_edges(combined, pos=pos)
-#     return plt.show()
