a73a7f87d14741c6b81cd5eb200ef554c094d97c
Andrew Mok <amokhtarzadeh22@gmail.com>
2022-12-05 23:38:36 -0800

    spectral

--- src/spectral_analysis.py
+++ src/spectral_analysis.py
@@ -8,8 +8,7 @@ def graph_stats(G):
     deg = []
     for i in G.degree:
         deg.append(i[1])
-    
-    
+
     stats = {
         'nodes': G.order(),
         'edges': G.number_of_edges(),
@@ -18,6 +17,16 @@ def graph_stats(G):
     }
     
     return stats
+
+def return_sub_graphs(G):
+    sub_graphs = {0:[], 1:[]}
+    for i in G.nodes:
+        G.subgraph(i)
+        sub_graphs[G.nodes[i]['value']].append(i)
+
+    for i in sub_graphs:
+        sub_graphs[i] = G.subgraph(sub_graphs[i])
+    return sub_graphs
 
 def spectral_cluster(G):
     A = nx.to_numpy_array(G)
@@ -31,13 +40,23 @@ def spectral_cluster(G):
     vals = vals[np.argsort(vals)]
     return [1 if i > 0 else 0 for i in vals]
 
-def return_sub_graphs(G)
-    sub_graphs = {0:[], 1:[]}
-    for i in G.nodes:
-        G.subgraph(i)
 
-        sub_graphs[G.nodes[i]['value']].append(i)
+def prediction_evaluation(G, pred_list):
+    # Returns accuracy
+    evaluate1 = []
+    evaluate2 = []
+    index = 0
+    for i in list(G.nodes(data = True)):
+        evaluate1.append(pred_list[index] == i[1]['value'])
+        evaluate2.append(pred_list[index] != i[1]['value'])
+        index += 1
 
-    for i in sub_graphs:
-        sub_graphs[i] = G.subgraph(sub_graphs[i])
-    return sub_graphs
+    return max(sum(evaluate1)/len(evaluate1), sum(evaluate2)/len(evaluate2))
+
+def spectral_evaluation(G):
+    preds = spectral_cluster(G)
+    vals = {
+        'predictions': spectral_cluster(G),
+        'accuracy': prediction_evaluation(G, preds)
+    }
+    return vals
